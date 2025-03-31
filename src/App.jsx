417f5cd387335239b5551126a8840fb996f043f8
sepehrshahinfar <sepehr.shahinfar@gmail.com>
2025-03-31 11:09:13 -0700

    Initial commit with React EventApp  project

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
+
+import Events from './components/Events/Events.jsx';
+import EventDetails from './components/Events/EventDetails.jsx';
+import NewEvent from './components/Events/NewEvent.jsx';
+import EditEvent, { loader as editLoader, action as editAction } from './components/Events/EditEvent.jsx';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './util/http.js';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/events" />,
+  },
+  {
+    path: '/events',
+    element: <Events />,
+
+    children: [
+      {
+        path: '/events/new',
+        element: <NewEvent />,
+      },
+    ],
+  },
+  {
+    path: '/events/:id',
+    element: <EventDetails />,
+    children: [
+      {
+        path: '/events/:id/edit',
+        element: <EditEvent />,
+        loader: editLoader,
+        action: editAction
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <QueryClientProvider client={queryClient}><RouterProvider router={router} /></QueryClientProvider>;
+}
+
+export default App;
